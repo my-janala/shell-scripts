6313577c06b448578af7968ae74efa22956f0880
mezbaur <mezbaur@yahoo.co.uk>
2013-06-21 11:51:09 +0200

    Update check-update.sh

--- check-update.sh
+++ check-update.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-
 yumtmp="/tmp/yum-check-update.$$"
 yum="/usr/bin/yum"
 
-yum check-update >& $yumtmp
-number=$(cat $yumtmp | egrep '(.i386|.x86_64|.noarch|.src)' | wc -l)
-#yumstatus="$?"
+#/sbin/runuser -s /bin/sh -m root -c "/usr/bin/yum check-update >& $yumtmp"
+
+sudo /bin/su - root -c "/usr/bin/yum check-update > /tmp/yum-check-update"
+#sudo /bin/su - root -c "/usr/bin/yum check-update > $yumtmp"
+#yum check-update >& $yumtmp
+
+number=$(cat /tmp/yum-check-update | egrep '(.i386|.x86_64|.noarch|.src)' | wc -l)
+#number=$(cat $yumtmp | egrep '(.i386|.x86_64|.noarch|.src)' | wc -l)
+#number=`cat $yumtmp | wc -l`
+#number=2
 
 case $number in
 0)
 echo "OK- No updates available"
 exit 0
 ;;
-
+1)
+echo "Warning- One update available"
+exit 1
+;;
 *)
 echo "Critical - $number updates available "
-exit 1
+exit 2
+;;
 
 esac
+
