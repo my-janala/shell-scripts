1bdd1befa4a9e2b86a747663f7d8bbf41a6757ac
mezbaur <mezbaur@yahoo.co.uk>
2013-12-07 11:09:19 +0000

    Create host-info.sh
    This is a menu driven script to display system related to a logged in user.

---
+++ host-info.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# case-menu: A case driven system information program
+
+clear
+echo "
+Please select:
+
+A. Display System Information
+B. Display Disk Space
+C. Display Progarms listening on ports
+D. Display IP Addresses
+E. Display Kernel Configuration
+F. Display Kernel Messages (dmesg)
+Q. Quit
+"
+
+read -p  "Enter selection [A, B, C, D, E, F or Q ] >"
+
+case $REPLY in 
+        q|Q)  echo "Program terminated"
+              exit 
+              ;;
+        a|A)  echo "Hostname : $HOSTNAME"
+              uptime
+              ;;
+        b|B)  df -h
+              ;;
+        c|C)  netstat -ntlp
+              ;;
+        d|D)  ifconfig 
+              ;;
+        e|E)  sysctl -A
+              ;;
+        f|F)  dmesg
+              ;;
+        *)    echo "Invalid entry" >&2
+              exit 1
+              ;;
+
+              
+              
+              
+
+
+"
